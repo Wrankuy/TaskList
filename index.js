bd80493fadb06d1ffb5c9d5c6c69b17739a89529
Wrankuy <32659614+Wrankuy@users.noreply.github.com>
2020-08-23 19:18:09 -0500

    src/ files
    pending DoneList Update

---
+++ index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Typography from '@material-ui/core/Typography';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import useTodoState from './useTodoState';
+import './styles.css';
+
+const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
+  return (
+    <div className="App">
+      <h1>
+        TO-DO LIST
+      </h1>
+      <TodoForm
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }
+      }
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      <h1>
+      DONE LIST
+      </h1>
+    </div>
+
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
